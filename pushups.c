b382e97f70ccf9a66a9f362a7d2fb7e0b1f29809
Abhijay Saini <abhijay.saini@gmail.com>
2021-02-08 09:27:02 -0600

    fixed localtime static bug

--- pushups.c
+++ pushups.c
@@ -9,20 +9,23 @@ int main(int argc, char* args[])
 	char* file_path = "pushups.dat"; // TODO: make universal
 	FILE *file = fopen(file_path, "r"); // TODO: simplify
 	time_t now = time(NULL);
-	struct tm *now_tm = localtime(&now);
-	// printf("Today: %d (%jd)\n", now_tm->tm_mday, now);
+	struct tm now_tm = *localtime(&now);
 
 	// read from file
 	uint8_t pushups = 0;
 	time_t prev = 0;
-	struct tm *prev_tm;
+	struct tm prev_tm;
 	fscanf(file, "%hhu\n%jd", &pushups, &prev);
 	fclose(file);
-	prev_tm = localtime(&prev); // TODO: better solution maybe? string parsing perhaps
-	// printf("Last modified: %d (%jd)\n", prev_tm->tm_mday, prev);
+	prev_tm = *localtime(&prev); // TODO: better solution maybe? string parsing perhaps
 
-	if (now_tm->tm_mday != prev_tm->tm_mday) // TODO: do better checking
+	if (now_tm.tm_mday != prev_tm.tm_mday) // TODO: do better checking
+	{
 		pushups = 0;
+	}
+
+	printf("Today: %d (%jd)\n", now_tm.tm_mday, now);
+	printf("Last modified: %d (%jd)\n", prev_tm.tm_mday, prev);
 
 	if (argc == 1)
 	{
